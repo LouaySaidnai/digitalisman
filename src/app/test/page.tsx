fd9e0f4c1932c346dfb8dfe65d1cd6b87a3f1fa4
Malek <malek.azri@insat.ucar.tn>
2025-07-02 21:45:15 +0100

    ...

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import EntrepreneurialTest from '../EntrepreneurialTest' // si tu l’as dans /components
+import EntrepreneurialTest from '../EntrepreneurialTest' // si tu l'as dans /components
 
 export default function TestPage() {
   return <EntrepreneurialTest />;
